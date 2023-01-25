122721537f7abdc3c0ef5df1e157467068c54aaa
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2023-01-25 19:39:30 +0900

    Update serializers.py

--- shinhan_toy/order/serializers.py
+++ shinhan_toy/order/serializers.py
@@ -1,14 +1,8 @@
 from rest_framework import serializers
-from django.contrib.auth.hashers import make_password
 from .models import Order
 
 class OrderSerializer(serializers.ModelSerializer):
-    def validate_password(self, value):
-        if len(value) < 8: # 비밀번호가 짧을시
-            raise serializers.ValidationError('Too short password')
-        # 유효성 검사가 끝난 값을 반환
-        return make_password(value)
 
     class Meta:
         model = Order
-        fields='__all__'
+        fields = '__all__'
