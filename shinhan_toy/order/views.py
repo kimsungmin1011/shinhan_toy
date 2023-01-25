acfadcb9c384b75b2e76a8826ea578d1f2b68cbf
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2023-01-25 19:48:01 +0900

    Update views.py

--- shinhan_toy/order/views.py
+++ shinhan_toy/order/views.py
@@ -6,10 +6,17 @@ from .models import Order
 from .serializers import OrderSerializer
 
 class Orderview(
+    mixins.ListModelMixin,
     mixins.CreateModelMixin,
     generics.GenericAPIView,
 ):
     serializer_class=OrderSerializer
+    
+    def get_queryset(self):
+        return Order.objects.all().order_by('id')
 
+    def get(self, request, *args, **kwargs):
+        return self.list(request, args, kwargs)
+    
     def post(self, request, *args, **kwargs):
         return self.create(request, args, kwargs)
